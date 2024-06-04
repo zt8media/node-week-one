6a7fd3aeb92eb352dc1cb130b28bf966bcc84555
zt8media <zt8media@gmail.com>
2024-06-04 12:32:41 -0400

    added comments

--- my-first-async-io.js
+++ my-first-async-io.js
@@ -18,16 +18,19 @@ fs.readFile(filePath, 'utf8', (err, data) => {
 });
 
 
-'use strict'
-const fs = require('fs')
-const file = process.argv[2]
 
-fs.readFile(file, function (err, contents) {
-  if (err) {
-    return console.log(err)
-  }
-  // fs.readFile(file, 'utf8', callback) can also be used
-  const lines = contents.toString().split('\n').length - 1
-  console.log(lines)
-})
+
+// Their solution 
+// 'use strict'
+// const fs = require('fs')
+// const file = process.argv[2]
+
+// fs.readFile(file, function (err, contents) {
+//   if (err) {
+//     return console.log(err)
+//   }
+//   // fs.readFile(file, 'utf8', callback) can also be used
+//   const lines = contents.toString().split('\n').length - 1
+//   console.log(lines)
+// })
 
--- my-first-io.js
+++ my-first-io.js
@@ -14,12 +14,16 @@ const numberOfNewlines = fileContent.split('\n').length - 1;
 console.log(numberOfNewlines);
 
 
-'use strict'
-    const fs = require('fs')
+
+
+
+// Thier solution 
+// 'use strict'
+//     const fs = require('fs')
     
-    const contents = fs.readFileSync(process.argv[2])
-    const lines = contents.toString().split('\n').length - 1
-    console.log(lines)
+//     const contents = fs.readFileSync(process.argv[2])
+//     const lines = contents.toString().split('\n').length - 1
+//     console.log(lines)
     
     // note you can avoid the .toString() by passing 'utf8' as the
     // second argument to readFileSync, then you'll get a String!
